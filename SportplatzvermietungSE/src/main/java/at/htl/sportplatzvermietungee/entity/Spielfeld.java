c549266c4beeb88e3d2b4643b3abaeb71c8e01bc
davidandraschko <d.andraschko@students.htl-leonding.ac.at>
2019-10-31 11:02:09 +0100

    Entity Spielfeld updated

--- SportplatzvermietungSE/src/main/java/at/htl/sportplatzvermietungee/entity/Spielfeld.java
+++ SportplatzvermietungSE/src/main/java/at/htl/sportplatzvermietungee/entity/Spielfeld.java
@@ -1,4 +1,48 @@
 package at.htl.sportplatzvermietungee.entity;
 
+
+import javax.persistence.*;
+
+@Entity
+@NamedQueries(@NamedQuery(name = "GetAll", query = "select s from Spielfeld s where s.id=?1"))
 public class Spielfeld {
+
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long SpielfeldId;
+    private int laenge;
+    private int breite;
+
+    public Spielfeld(int laenge, int breite) {
+        this.setLaenge(laenge);
+        this.setBreite(breite);
+    }
+
+    public Spielfeld(){
+
+    }
+
+
+    public Long getSpielfeldId() {
+        return SpielfeldId;
+    }
+
+
+
+    public int getLaenge() {
+        return laenge;
+    }
+
+    public void setLaenge(int laenge) {
+        this.laenge = laenge;
+    }
+
+    public int getBreite() {
+        return breite;
+    }
+
+    public void setBreite(int breite) {
+        this.breite = breite;
+    }
+}
+
 }
