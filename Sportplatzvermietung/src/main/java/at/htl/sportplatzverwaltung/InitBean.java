00d8046bab64c5ec1e54ccfd69303b2657ae38a1
davidandraschko <d.andraschko@students.htl-leonding.ac.at>
2019-11-20 17:30:52 +0100

    Tried two Lamda Expressions

--- Sportplatzvermietung/src/main/java/at/htl/sportplatzverwaltung/InitBean.java
+++ Sportplatzvermietung/src/main/java/at/htl/sportplatzverwaltung/InitBean.java
@@ -9,6 +9,9 @@ import javax.enterprise.event.Observes;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
 
 @ApplicationScoped
 @Transactional
@@ -23,6 +26,17 @@ public class InitBean {
         em.persist(new Spielfeld(120, 90));
         em.persist(new Spielfeld(100, 80));
         em.persist(new Spielfeld(90, 45));
+
+        List<Integer> lamdaExpressionTry = new ArrayList<>();
+        for(int i = 0; i < 30; i++){
+            lamdaExpressionTry.add(i);
+        }
+
+        Function<String, String> f1 = (s) -> s.toLowerCase();
+        System.out.println(f1.apply("Test Text! "));
+
+        lamdaExpressionTry.removeIf(i -> i % 2 == 0);
+        lamdaExpressionTry.forEach(i -> System.out.println(i + " "));
     }
 
     public void tearDown(@Observes @Destroyed(ApplicationScoped.class) Object init){
